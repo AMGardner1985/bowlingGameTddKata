95e827363af266781ef97dc8aa798cead476200e
Adam Gardner <AdamGardner33@gmail.com>
2019-03-31 20:52:38 -0400

    Added logic for scoring spares

--- BowlingGame/BowlingGame/Game.cs
+++ BowlingGame/BowlingGame/Game.cs
@@ -4,17 +4,39 @@ namespace BowlingGame
 {
     public class Game
     {
-        private int score = 0;
+        
+        private int[] rolls = new int[21];
 
+        private int currentRoll = 0;
 
         public void roll(int pins)
         {
-            score += pins;   
+            rolls[currentRoll++] = pins;
         }
 
         public int getScore()
         {
+            int score = 0;
+            int firstInFrame = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isSpare(firstInFrame))
+                {
+                    score += 10 + rolls[firstInFrame + 2];
+                    firstInFrame += 2;
+                }
+                else
+                {
+                    score += rolls[firstInFrame] + rolls[firstInFrame + 1];
+                    firstInFrame += 2;
+                }
+            }
             return score;
+        }
+
+        private bool isSpare(int firstInFrame)
+        {
+            return rolls[firstInFrame] + rolls[firstInFrame + 1] == 10;
         }
     }
 }
--- BowlingGame/BowlingGameTests/BowlingTest.cs
+++ BowlingGame/BowlingGameTests/BowlingTest.cs
@@ -7,6 +7,12 @@ namespace BowlingTest
     {
         private Game g;
 
+        private void rollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+                g.roll(pins);
+        }
+
         [SetUp]
         public void Setup()
         {
@@ -15,25 +21,35 @@ namespace BowlingTest
 
 
         [Test]
-        public void canRoll()
+        public void CanRoll()
         {
             g.roll(0);
         }
 
         [Test]
-        public void canRoleGutterGame()
+        public void CanRoleGutterGame()
         {
-            for (int i = 0; i < 20; i++)
-                g.roll(0);
+            rollMany(20, 0);
             Assert.AreEqual(0, g.getScore());
         }
 
+
         [Test]
-        public void allOnes()
+        public void AllOnes()
         {
-            for (int i = 0; i < 20; i++)
-                g.roll(1);
+            rollMany(20, 1);
             Assert.AreEqual(20, g.getScore());
+        }
+
+        //[Ignore("")]
+        [Test]
+        public void OneSpare()
+        {
+            g.roll(5);
+            g.roll(5); //spare
+            g.roll(3);
+            rollMany(17,0);
+            Assert.AreEqual(16, g.getScore());
         }
     }   
 }
